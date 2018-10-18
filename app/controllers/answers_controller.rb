3e0f808460b7353c714dd7961fca094dd48f857f
Troy Leach <troyleach29@gmail.com>
2018-10-18 16:09:27 -0400

    testing

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,9 @@
 class AnswersController < ApplicationController
   def get_answer
-
+    text = 'this is some text for you'
+    user = {"troy": "leach"}
+    puts params
+    #render json: user
+    render body: 'this is some text for you'
   end
 end
